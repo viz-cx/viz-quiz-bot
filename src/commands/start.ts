ce96230bd90fb22662a2be4ab195685b5644a3b2
Vladimir Babin <vovababin@gmail.com>
2020-07-26 15:04:38 +0500

    Update quiz logic

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,4 +1,5 @@
 import { Telegraf, Context } from "telegraf"
+import { nextQuestionKeyboard } from "../middlewares/checkAnswer"
 
 export function setupStart(bot: Telegraf<Context>) {
     bot.command(['start'], ctx => {
@@ -6,39 +7,84 @@ export function setupStart(bot: Telegraf<Context>) {
     })
 }
 
-function sendQuiz(ctx: Context) {
+export function sendQuiz(ctx: Context) {
     let Airtable = require('airtable')
-        Airtable.configure({
-            endpointUrl: 'https://api.airtable.com',
-            apiKey: process.env.AIRTABLE_API_KEY
-        })
-        let base = Airtable.base(process.env.AIRTABLE_BASE)
-        base('RU').select({
-            maxRecords: 1200,
-            view: 'Grid view'
-        }).firstPage(function (err, records) {
-            if (err) {
-                console.error(err)
-                return
+    Airtable.configure({
+        endpointUrl: 'https://api.airtable.com',
+        apiKey: process.env.AIRTABLE_API_KEY
+    })
+    let base = Airtable.base(process.env.AIRTABLE_BASE)
+    base('RU').select({
+        maxRecords: 1200,
+        view: 'Grid view'
+    }).firstPage(function (err, records) {
+        if (err) {
+            console.error(err)
+            return
+        }
+        let filteredRecords = records.filter(function (record) {
+            let verified = record.get('Verified')
+            if (verified != true) {
+                return false
             }
-            let filteredRecords = records.filter(function (record) {
-                return record.get('Question') != undefined
-            })
-            let randomRecord = filteredRecords[Math.floor(Math.random() * filteredRecords.length)]
-            let question = randomRecord.get('Question')
-            let answers = randomRecord.get('Answers').split(/\n/).filter(n => n)
-            let correctValue = answers[0]
-            let shuffledAnswers = shuffle(answers)
-            let correctValueID = shuffledAnswers.indexOf(correctValue)
-            // let secondsToAnswer = 10
-            ctx.replyWithQuiz(question, shuffledAnswers, {
-                is_anonymous: false,
-                allows_multiple_answers: false,
-                correct_option_id: correctValueID,
-                is_closed: false,
-                // open_period: secondsToAnswer
-            })
+            let q = record.get('Question')
+            if (q == undefined) {
+                return false
+            }
+            if (q.length < 1 || q.length > 255) {
+                console.log('Вопрос ', q, ' не удовлетворяет условиям длины')
+                return false
+            }
+            let e = record.get('Explanation')
+            if (e.length > 200) {
+                console.log('Ответ к вопросу', q, 'слишком длинный')
+                return false
+            }
+            return true
         })
+        let answeredRecords = ctx.dbuser.answeredRecords
+        let unansweredRecords = filteredRecords
+            .filter((record) => answeredRecords.indexOf(record.id) === -1)
+        if (unansweredRecords.length === 0) {
+            let replyMsg = ctx.reply("Закончились неотвеченные вопросы, попробуйте завтра", {reply_markup: nextQuestionKeyboard})
+            replyMsg.then(msg => {
+                let user = ctx.dbuser
+                if (user.quizMessageId) {
+                    ctx.deleteMessage(user.quizMessageId)
+                }
+                user.quizMessageId = msg.message_id
+                user.save()
+            })
+            return
+        }
+        let randomRecord = unansweredRecords[Math.floor(Math.random() * unansweredRecords.length)]
+        let question = randomRecord.get('Question')
+        let answers = randomRecord.get('Answers').split(/\n/).filter(n => n)
+        let correctValue = answers[0]
+        let shuffledAnswers = shuffle(answers)
+        let correctValueID = shuffledAnswers.indexOf(correctValue)
+        let explanation = randomRecord.get('Explanation')
+        // let secondsToAnswer = 10
+        let quizMsg = ctx.replyWithQuiz(question, shuffledAnswers, {
+            is_anonymous: true,
+            allows_multiple_answers: false,
+            correct_option_id: correctValueID,
+            is_closed: false,
+            explanation: explanation,
+            // open_period: secondsToAnswer
+        })
+        quizMsg.then(msg => {
+            let user = ctx.dbuser
+            if (user.quizMessageId) {
+                ctx.deleteMessage(user.quizMessageId)
+            }
+            user.answeredRecords.push(randomRecord.id)
+            user.quizId = msg.poll.id
+            user.quizMessageId = msg.message_id
+            user.save()
+            // setTimeout(function () { closePoll(ctx, msg.message_id) }, secondsToAnswer * 1000);
+        })
+    })
 }
 
 function shuffle(array) {
--- src/middlewares/checkAnswer.ts
+++ src/middlewares/checkAnswer.ts
@@ -1,19 +1,35 @@
 import { Context } from 'telegraf'
+import { sendQuiz } from '../commands/start'
+
+export const nextQuestionKeyboard = {
+    inline_keyboard: [[{ text: "Следующий вопрос", callback_data: "next_question" }]]
+}
 
 export async function checkAnswer(ctx: Context, next: () => any) {
-    console.log(ctx)
+    if (ctx.callbackQuery && ctx.callbackQuery.data == 'next_question') {
+        sendQuiz(ctx)
+    }
     if (ctx.updateType === 'poll') {
-        console.log(ctx.poll)
         let options = ctx.poll.options
         var answerID = -1
+        var allVotesCount = 0
         for (let i = 0; i < options.length; i++) {
-            if (options[i].voter_count === 1) {
+            let voterCount = options[i].voter_count
+            allVotesCount += voterCount
+            if (voterCount == 1) {
                 answerID = i
             }
         }
         let isCorrectAnswer = (answerID == ctx.poll.correct_option_id)
-        console.log(isCorrectAnswer)
+        if (!ctx.poll.is_closed && allVotesCount == 1 && isCorrectAnswer) {
+            console.log('TODO: award!')
+        }
+        addNextQuestionButton(ctx)
     } else {
         next()
     }
+}
+
+function addNextQuestionButton(ctx: Context) {
+    ctx.telegram.editMessageReplyMarkup(ctx.dbuser.id, ctx.dbuser.quizMessageId, undefined, nextQuestionKeyboard)
 }
