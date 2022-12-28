f34ae5aba2f59b899b3648500e7b669c27f4a037
Vladimir Babin <vovababin@gmail.com>
2022-12-28 12:06:06 +0500

    Remove creation viz instance for every request

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -2,8 +2,10 @@ import { VIZ } from '@/helpers/viz'
 import { findUserByPollId, getOrCreateUser } from '@/models/index'
 import { Context } from 'telegraf'
 
+const viz = new VIZ()
+
 export async function attachUser(ctx: Context, next) {
-  ctx.viz = new VIZ()
+  ctx.viz = viz
   if (ctx.poll) {
     ctx.dbuser = await findUserByPollId(ctx.poll.id)
     if (ctx.dbuser !== null) {
